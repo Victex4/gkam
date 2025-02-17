5274379fbb296ad7f7807c8a23a90ec5952c413c
Victex4 <eberevictory3@gmail.com>
2025-02-17 05:55:14 +0100

    initial commit

--- src/section/User.jsx
+++ src/section/User.jsx
@@ -6,13 +6,13 @@ const Login = ({ handleLoginPopUp, handleAccPopUp }) => {
     <section className='bg-[#FFE9FE]'>
       <div className='container flex gap-4 items-center pt-8 md:pt-0 justify-center'>
         <div  data-aos="fade-up animate-bounce-up" >
-          <p className="px-20 animate-bounce-up bg-white py-8 rounded-2xl text-2xl font-semibold" >
+          <p className="md:px-20 animate-bounce-up px-10 py-4 bg-white md:py-8 rounded-2xl md:text-2xl font-semibold" >
             30,000+ <span>Orders</span>
           </p>
         </div>
         {/* <hr className='border-t-2 border-black/80 text-center' style={{ width: '10%', margin: '0 auto'}} /> */}
         <div  data-aos="fade-up" >
-          <p className="px-20 animate-bounce-up bg-white py-8 rounded-2xl text-2xl font-semibold" >
+          <p className="md:px-20 px-10 py-4 animate-bounce-up bg-white md:py-8 rounded-2xl md:text-2xl font-semibold" >
             10,000+ <span>Users</span>
           </p>
         </div>
