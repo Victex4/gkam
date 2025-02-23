09560caff41afea557cfe6e916c0d63b99e2babc
Victex4 <eberevictory3@gmail.com>
2025-02-23 23:29:13 +0100

    initial commit

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -9,7 +9,7 @@ const HeroImage = ({ variants }) => {
   return (
     <motion.div> 
       <div className='absolute md:right-0 top-0 translate-y-1/2 mt-[12rem] md:w-[350px] w-[250px] h-[250px] md:h-[350px] rounded-full border-[#EB10E8] border-[32px] z-10' data-aos="fade-up"></div>
-      <img className='absolute animate-bounce-up  top-0 md:left-1/2 md:top-0 mt-[6rem] w-[1200px] md:w-[800px] right-0 z-20' data-aos="fade-up" src={userImage} alt="User Image" />
+      <img className='absolute animate-bounce-up left-11 top-0 md:left-1/2 md:top-0 mt-[6rem] w-[1200px] md:w-[800px] right-0 z-20' data-aos="fade-up" src={userImage} alt="User Image" />
       <img src={blob} alt="" />
     </motion.div>
 
